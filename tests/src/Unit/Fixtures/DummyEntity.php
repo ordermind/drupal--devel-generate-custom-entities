00aa26879774bea8b55c0ab276d5383567cb99d8
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-07-31 18:14:32 +0200

    Add test for generating a single entity

--- tests/src/Unit/Fixtures/DummyEntity.php
+++ tests/src/Unit/Fixtures/DummyEntity.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\devel_generate_custom_entities\Unit\Fixtures;
 
-use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityBase;
 
-class DummyEntity extends ContentEntityBase {
+class DummyEntity extends EntityBase {
+  protected int $id;
+  protected string $label;
+
+  protected string $bundle;
+  protected ?int $uid;
+  protected int $status;
+  protected int $created;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function bundle(): string {
+    return $this->bundle;
+  }
+
+  public function uid(): ?int {
+    return $this->uid;
+  }
 
 }
--- tests/src/Unit/Generator/EntityGeneratorTest.php
+++ tests/src/Unit/Generator/EntityGeneratorTest.php
@@ -5,66 +5,74 @@ declare(strict_types=1);
 namespace Drupal\Tests\devel_generate_custom_entities\Unit\Generator;
 
 use Drupal\Component\Datetime\Time;
-use Drupal\Core\Cache\MemoryCache\MemoryCache;
-use Drupal\Core\Cache\NullBackend;
-use Drupal\Core\Entity\ContentEntityNullStorage;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityType;
-use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\devel_generate_custom_entities\Generator\EntityGenerator;
 use Drupal\devel_generate_custom_entities\ValueObject\EntityGenerationOptions;
 use Drupal\Tests\devel_generate_custom_entities\Unit\Fixtures\DummyEntity;
 use Drupal\Tests\UnitTestCase;
+use Ordermind\DrupalTengstromShared\Test\Concerns\AddsServicesToContainerDuringTest;
+use Ordermind\DrupalTengstromShared\Test\Fixtures\EntityStorage\EntityArrayStorage;
+use Ordermind\DrupalTengstromShared\Test\Fixtures\Factories\TestServiceContainerFactory;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 
 class EntityGeneratorTest extends UnitTestCase {
   use ProphecyTrait;
+  use AddsServicesToContainerDuringTest;
 
-  /**
-   * @dataProvider provideTestGenerateSingleEntity
-   */
-  public function testGenerateSingleEntity(EntityGenerationOptions $options): void {
+  protected function setUp(): void {
+    parent::setUp();
+
+    $containerFactory = new TestServiceContainerFactory();
+    $container = $containerFactory->createWithBasicServices();
+
+    \Drupal::setContainer($container);
+  }
+
+  public function testGenerateSingleEntity(): void {
+    $options = new EntityGenerationOptions(FALSE, 'test_type', 'Test Type #@num', ['bundle_1'], 0, FALSE, 1);
+
     $entityType = new EntityType([
-      'id' => 'test_type',
+      'id' => $options->getEntityTypeId(),
       'class' => DummyEntity::class,
+      'entity_keys' => [
+        'id' => 'id',
+        'label' => 'label',
+      ],
     ]);
 
-    $mockEntityFieldManager = $this->prophesize(EntityFieldManagerInterface::class);
-    $entityFieldManager = $mockEntityFieldManager->reveal();
-
-    $cacheBackend = new NullBackend('entity');
-    $memoryCache = new MemoryCache();
-
-    $mockEntityTypeBundleInfo = $this->prophesize(EntityTypeBundleInfoInterface::class);
-    $entityTypeBundleInfo = $mockEntityTypeBundleInfo->reveal();
-
-    $storage = new ContentEntityNullStorage(
-      $entityType,
-      $entityFieldManager,
-      $cacheBackend,
-      $memoryCache,
-      $entityTypeBundleInfo
-    );
+    $storage = EntityArrayStorage::createInstance(\Drupal::getContainer(), $entityType);
 
     $mockEntityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
-    $mockEntityTypeManager
-      ->getStorage(Argument::any())->willReturn($storage);
+    $mockEntityTypeManager->getStorage(Argument::any())->willReturn($storage);
+    $mockEntityTypeManager->getDefinition($entityType->id())->willReturn($entityType);
     $entityTypeManager = $mockEntityTypeManager->reveal();
 
     $mockTimeService = $this->prophesize(Time::class);
     $mockTimeService->getRequestTime()->willReturn(1690208874);
     $timeService = $mockTimeService->reveal();
 
-    $entityGenerator = new EntityGenerator($entityTypeManager, $timeService);
-    dump($entityGenerator->generateSingleEntity($options, 1));
-  }
+    $this->addMultipleServices([
+      'entity_type.manager' => $entityTypeManager,
+    ]);
 
-  public function provideTestGenerateSingleEntity(): array {
-    return [
-      [new EntityGenerationOptions(FALSE, 'test_type', 'Test Type #@num', ['bundle_1'], 0, FALSE, 1)],
-    ];
+    $this->assertEquals($storage->count(), 0);
+
+    $entityGenerator = new EntityGenerator($entityTypeManager, $timeService);
+    $entityGenerator->generateSingleEntity($options, 1);
+
+    $this->assertEquals($storage->count(), 1);
+
+    $entity = $storage->load(1);
+    $this->assertInstanceOf(DummyEntity::class, $entity);
+    /** @var \Drupal\Tests\devel_generate_custom_entities\Unit\Fixtures\DummyEntity $entity */
+
+    $this->assertSame($options->getEntityTypeId(), $entity->getEntityType()->id());
+    $this->assertSame($options->getEntityTypeId(), $entity->getEntityTypeId());
+    $this->assertSame(str_replace('@num', (string) $entity->id(), $options->getLabelPattern()), $entity->label());
+    $this->assertSame($options->getBundleNames(), [$entity->bundle()]);
+    $this->assertSame($options->getAuthorUid(), $entity->uid());
   }
 
 }
